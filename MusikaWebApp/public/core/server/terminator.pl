f027573bef10435c77c0f4a6dc54a1b1e1aa6163
lc991 <lorenzo.cozza@hotmail.com>
2015-07-03 18:33:13 +0200

    terminator script updated: it takes in input a keywords and give a list of video

--- MusikaWebApp/public/core/server/terminator.pl
+++ MusikaWebApp/public/core/server/terminator.pl
@@ -1,22 +1,49 @@
 use strict;
 use warnings;
 
-open(FILE_YT, $ARGV[0]);
-my @fileYt = <FILE_YT>;
+my $htmlFile = 'research';
 my @videosYT = ();
-close (FILE_YT);
+
+print `wget -O $htmlFile https://www.youtube.com/results?search_query=$ARGV[0]`."\n";
+my @fileYt = `cat $htmlFile`;
+
+#consts
+my $__ID     = 0;
+my $__IDYT   = 1;
+my $__TITLE  = 2;
+my $__imgUrl = 3;
+
+my $i=0;
 foreach my $line (@fileYt){
-	chomp($line);
-	if( $line =~ m/class\=\"yt\-lockup\-content\"/ ){
-		if( $line =~ m/\<a href\=\"\/watch\?v\=(.+?)\"/ ){
-			my $id = $1;
-			print "$1  ";
-			if( $line =~ m/title\="(.+?)"/ ){
-				print "$1  ";
-				if( $line =~ m/img src\="(.+$id.*?)"/ ){
-					print "$1\n";	
-				}
-			}
-		}
-	}
+    chomp($line);
+    my $find = 0;
+    my @tmp;
+    if( $line =~ m/class\=\"yt\-lockup\-content\"/ ){
+        if( $line =~ m/\<a href\=\"\/watch\?v\=(.+?)\"/ ){
+            $find=1;
+            $tmp[$__ID] = $i++;
+            $tmp[$__IDYT] = $1;
+            if( $line =~ m/title\="(.+?)"/ ){
+                $tmp[$__TITLE] = $1;
+            }
+        }
+    }
+    if ($find==1){
+        push(@videosYT, [@tmp]);
+    }
 }
+
+foreach my $video (@videosYT){
+    my $idYT = ${$video}[$__IDYT];
+    chomp($idYT);
+    foreach my $line (@fileYt){
+        if( $line =~ m/\="(\/\/.+$idYT.+\.jpg)"/ ){
+                ${$video}[$__imgUrl] = $1;
+        }
+    }
+
+}
+
+foreach my $video (@videosYT){
+    print "${$video}[$__ID] ${$video}[$__IDYT] ${$video}[$__TITLE] ${$video}[$__imgUrl]\n";
+} 
